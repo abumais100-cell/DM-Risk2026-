ad072ca437b179dc2091e2817f6dd8d88dbf9c43
abumais100-cell <abumais100@gmail.com>
2026-01-20 12:13:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = "dar-risk-2026-v1";
+const BASE = "/DM-Risk2026";
+const CACHE_NAME = "dar-risk-2026-v2";
+
 const ASSETS = [
-  "/DM-Risk2026/",
-  "/DM-Risk2026/index.html",
-  "/DM-Risk2026/manifest.webmanifest",
-  "/DM-Risk2026/icons/icon-192.png",
-  "/DM-Risk2026/icons/icon-512.png"
+  `${BASE}/`,
+  `${BASE}/index.html`,
+  `${BASE}/manifest.webmanifest`,
+  `${BASE}/icons/icon-192.png`,
+  `${BASE}/icons/icon-512.png`
 ];
 
 self.addEventListener("install", (event) => {
